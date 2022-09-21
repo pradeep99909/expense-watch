d598e618ef70ba72e71a77651519d26f91ebb465
Pradeep Kshirsagar <30392189+pradeep99909@users.noreply.github.com>
2022-09-21 17:20:34 +0000

    bills add changes

--- src/Pages/Bills/bill.tsx
+++ src/Pages/Bills/bill.tsx
@@ -116,117 +116,59 @@ const Modal = () => {
         <FontAwesomeIcon icon={faAdd} />
         &nbsp; Add
       </button>
-      <div
-        id="authentication-modal"
-        className={
-          display +
-          "dark overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center bg-black/80 shadow"
-        }
-      >
-        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-          <div className="relative bg-black-200 rounded-lg shadow dark:bg-gray-700">
-            <button
-              onClick={() => setDisplay("hidden ")}
-              type="button"
-              className="absolute top-3 right-2.5 text-white bg-transparent hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-            >
-              <svg
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <div className="py-6 px-6 lg:px-8">
-              <h3 className="mb-4 text-xl font-medium text-gray-100 dark:text-white">
-                Add Bills and Montly payment
+      <div id="editUserModal" tabIndex={2} aria-hidden="true" className={display +
+        "flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center p-4 w-full md:inset-0 h-modal md:h-full bg-black/80 dark"
+      }>
+        <div className="relative w-full max-w-2xl h-full md:h-auto">
+          <form action="#" className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Add Bills and Monthly Payments
               </h3>
-              <form className="space-y-6" action="#">
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">
-                    Bill or Payment
-                  </label>
-                  <input
-                    maxLength={20}
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="bg-black border border-gray-300 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="eg: Credit Card"
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">
-                    Type
-                  </label>
-                  <select
-                    className="text-sm font-medium"
-                    onChange={(e) => {
-                      setType(e.target.value);
-                    }}
-                    required
-                  >
-                    <option className="w-10 py-2">Loan</option>
-                    <option className="w-10 py-2">Credit Card</option>
-                    <option className="w-10 py-2">Subscribtion</option>
-                    <option className="w-10 py-2">Bill</option>
-                    <option className="w-10 py-2">Others</option>
-                  </select>
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">
-                    Amount
-                  </label>
-                  <input
-                    type="number"
-                    name="amount"
-                    id="password"
-                    placeholder="eg: 700"
-                    className="bg-black border border-gray-300 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    onChange={(e) => {
-                      setAmount(parseInt(e.target.value));
-                    }}
-                    required
-                  />
-                </div>
-                <button
-                  style={{
-                    backgroundImage:
-                      "linear-gradient(90deg, rgba(30,58,138,1) 0%, rgba(30,64,175,1) 10%, rgba(29,78,216,1) 100%, rgba(37,99,235,1) 100%)",
-                  }}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setDisplay("hidden ");
-                    setBills([
-                      ...bills,
-                      {
-                        name,
-                        type,
-                        amount,
-                      },
-                    ]);
-                    setName("");
-                    setAmount(0);
-                    setType("");
-                  }}
-                  type="submit"
-                  className="w-full text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Add
-                </button>
-              </form>
+              <button type="button" onClick={() => setDisplay("hidden ")} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="editUserModal">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              </button>
             </div>
-          </div>
+            <div className="p-6 space-y-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                  <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bill Name" required />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type</label>
+                  <input type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} id="type" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bill Type" required />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                  <input type="number" name="amount" value={amount} onChange={(e) => setAmount(Number(e.target.value))} id="amount" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+              <button type="submit" onClick={(e) => {
+                e.preventDefault();
+                if (name && type && amount > 0) {
+                  const bill = {
+                    name,
+                    type,
+                    amount,
+                  }
+                  setBills([
+                    ...bills,
+                    bill
+                  ])
+                  setName("");
+                  setType("");
+                  setAmount(0);
+                  setDisplay("hidden ")
+                }
+                else {
+                  alert("Add Values.")
+                }
+              }} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
