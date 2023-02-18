afd6ed83e0b5e2562fedd831142ac2a0c48d12c6
Pradeep Kshirsagar <pradeep99909@gmail.com>
2023-02-18 22:36:59 +0530

    switch changes

--- src/Pages/Bills/bill.tsx
+++ src/Pages/Bills/bill.tsx
@@ -375,10 +375,10 @@ class BillsDisplay extends React.Component<IMyComponentProps, IMyComponentState>
                     <input type="number" name="email" value={this.state.amount} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@company.com" required />
                   </div>
                   <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="green-toggle" className="inline-flex relative items-center mr-5 cursor-pointer">
+                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</span>
+                    <label htmlFor="green-toggle" className="inline-flex relative h-full mr-5 cursor-pointer">
                       <input type="checkbox" value="" id="green-toggle" className="sr-only peer" checked={this.state.active} />
                       <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-                      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Status</span>
                     </label>
                   </div>
                 </div>
